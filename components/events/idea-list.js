fff9b11661d2f773abdb7727d956e9c94a0cb133
Daniel Mendez <awilddanoru@gmail.com>
2022-09-18 17:30:12 -0700

    Added ideas for hackathon along with modular css

---
+++ components/events/idea-list.js
@@ -0,0 +1,35 @@
+import classes from "./idea-list.module.css"
+
+function IdeaList() {
+  return (
+    <div className={classes.container}>
+      <div className={classes.item}>
+        <h3>MoveIt</h3>
+        <h4>Movement Reminder</h4>
+        <p>A Chrome extension that gives you reminders to move/stretch throughout your work day and offers potential exercises via Youtube.</p>
+      </div>
+      <div className={classes.item}>
+        <h3>Post Death</h3>
+        <h4>End-of-Life Planner</h4>
+        <p>A web application that allows users to put together all of their end-of-life planning information in a single, convenient space for your loved ones to reference in the end.</p>
+      </div>
+      <div className={classes.item}>
+        <h3>A Quiet Place</h3>
+        <h4>Library Room Scheduler</h4>
+        <p>A web application that allows you to schedule a room to use in a library.</p>
+      </div>
+      <div className={classes.item}>
+        <h3>Karaoq</h3>
+        <h4>Karaoke Performance Queue</h4>
+        <p>A web application that handles karaoke open mic performance queues with multiple users and Youtube-provided karaoke videos.</p>
+      </div>
+      <div className={classes.item}>
+        <h3>Writer's Box</h3>
+        <h4>Hub for Randomly-Generated Content</h4>
+        <p>A web application with a variety of lists (town names, NPC names, store inventories, etc.) that can be used to quickly generate things a dungeonmaster/creative might need in a pinch.</p>
+      </div>
+    </div>
+  );
+};
+
+export default IdeaList;
--- pages/events/e1.js
+++ pages/events/e1.js
@@ -1,5 +1,6 @@
 import { getFeaturedUsers } from "../../user-data"
 import UserList from "../../components/users/user-list"
+import IdeaList from "../../components/events/idea-list"
 
 function EventPage2022() {
   const featuredUsers = getFeaturedUsers();
@@ -7,20 +8,13 @@ function EventPage2022() {
   return (
     <div>
       <div>
-        <h1>Hack in the Heights Fall 2022</h1>
+        <h1>Hack in the Heights: Fall 2022</h1>
+        <IdeaList />
+      </div>
+      <div>
         <UserList
           items={featuredUsers} 
         />
-      </div>
-      <div>
-        <ul>
-          <li>Test Stretch</li>
-          <li>Work Tracker</li>
-          <li>Post-Death</li>
-          <li>Library Room Booker</li>
-          <li>DIY Karaoke</li>
-          <li>Random Generator Hub</li>
-        </ul>
       </div>
     </div>
   )
